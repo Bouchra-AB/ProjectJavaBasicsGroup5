b2870fad704b313724444d9b7e4025fcd39bc2b6
Bouchra-AB <hopeswin0908@gmail.com>
2024-08-03 23:47:28 -0400

    commit changes

--- src/main/java/ProjecJavaBasicsG5/Q2/SumOfAllValuesInArray.java
+++ src/main/java/ProjecJavaBasicsG5/Q2/SumOfAllValuesInArray.java
@@ -1,14 +1,14 @@
 package ProjecJavaBasicsG5.Q2;
 
 public class SumOfAllValuesInArray {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //Create an array of integer values
         int[] values = {13, 56, 8, 90, 102, 5, 20, 11};
 
         int sum = 0;
 
         //Calculate the sum of all elements in the array
-        for (int num : values){
+        for (int num : values) {
             sum += num;
         }
 
--- src/main/java/ProjecJavaBasicsG5/Q7/RecursiveFibonacci.java
+++ src/main/java/ProjecJavaBasicsG5/Q7/RecursiveFibonacci.java
@@ -16,8 +16,8 @@ public class RecursiveFibonacci {
 
     // Recursive function to calculate Fibonacci numbers
     public static int fibonacciNumbers(int n) {
-        if (n==0) return 0;
-        if (n==1) return 1;
+        if (n == 0) return 0;
+        if (n == 1) return 1;
         return fibonacciNumbers(n - 1) + fibonacciNumbers(n - 2);
     }
 
